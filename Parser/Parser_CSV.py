c69971b426dafb269a7ef7796aa6d3491fdf5a2f
18411029 <rvvagner@sberbank.ru>
2021-02-11 15:52:56 +0600

    fix Parser_CSV.py

--- Parser/Parser_CSV.py
+++ Parser/Parser_CSV.py
@@ -1,11 +1,10 @@
 import csv
 
-
 with open("C:\\Users\\18411029\\Documents\\PythonCourses\\csv\\export_f.csv", 'r', newline="", encoding='utf-8') as file:
     # Считываем файл, указывая делитель ";"
     reader = csv.DictReader(file, delimiter=';')
     # Проходимся циклом по строке дата
-    with open("csv\\exp.html", "w", encoding="UTF-8") as p:
+    with open("C:\\Users\\18411029\\Documents\\PythonCourses\\csv\\exp.txt", "w", encoding="UTF-8") as p:
         for row in reader:
             # Выводим строки из поля 'Номер и название SC/FAQ' которые соотвествует Дате
             if row["Дата"] == '20.01.2021':
