b758a817f075df142e13fc75f00c74fccf653f65
18411029 <rvvagner@sberbank.ru>
2021-02-11 19:34:27 +0600

    Original file with all changes

--- Parser/Parser_CSV.py
+++ Parser/Parser_CSV.py
@@ -1,12 +1,12 @@
 import csv
 
-with open("C:\\Users\\18411029\\Documents\\PythonCourses\\csv\\export_f.csv", 'r', newline="", encoding='utf-8') as file:
+with open("export_r.csv", 'r', newline="",
+          encoding='utf-8') as file:
     # Считываем файл, указывая делитель ";"
     reader = csv.DictReader(file, delimiter=';')
     # Проходимся циклом по строке дата
-    with open("C:\\Users\\18411029\\Documents\\PythonCourses\\csv\\exp.txt", "w", encoding="UTF-8") as p:
+    with open("exp.txt", "w", encoding="UTF-8") as p:
         for row in reader:
             # Выводим строки из поля 'Номер и название SC/FAQ' которые соотвествует Дате
             if row["Дата"] == '20.01.2021':
                 p.write(row["Номер и название SC/FAQ"] + ' | ' + row["Описание изменений"] + "\n")
-
