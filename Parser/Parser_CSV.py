9952c0f1acb170547bc1e61803df9a655039db26
18411029 <rvvagner@sberbank.ru>
2021-02-11 20:10:40 +0600

    работа над сохранением отдельного файла

--- Parser/Parser_CSV.py
+++ Parser/Parser_CSV.py
@@ -1,18 +1,16 @@
 import csv
 import win32com.client as win32
 
-with open("C:\\Users\\18411029\\Documents\\PythonCourses\\csv\\export_f.csv", 'r', newline="", encoding='utf-8') as file:
+with open("export_r.csv", 'r', newline="", encoding='utf-8') as file:
     # Считываем файл, указывая делитель ";"
     reader = csv.DictReader(file, delimiter=';')
     # Проходимся циклом по строке дата
-    with open("C:\\Users\\18411029\\Documents\\PythonCourses\\csv\\exp.csv", "w", encoding="UTF-8", newline="") as p:
-        fieldnames = ['Номер и название SC/FAQ', 'Описание изменений']
-        writer = csv.DictWriter(p, fieldnames=fieldnames, delimiter=';')
-        for row in reader:
-            # Выводим строки из поля 'Номер и название SC/FAQ' которые соотвествует Дате
-            if row["Дата"] == '03.02.2021':
-                # writer.writeheader()
-                writer.writerow([row["Номер и название SC/FAQ"], row["Описание изменений"]])
+    r = file
+with open("exp.txt", "w", encoding="UTF-8") as p:
+    for row in r:
+        # Выводим строки из поля 'Номер и название SC/FAQ' которые соотвествует Дате
+        if row["Дата"] == '20.01.2021':
+            p.write(row["Номер и название SC/FAQ"] + ' | ' + row["Описание изменений"] + "\n")
 
 # outlook = win32.Dispatch('Outlook.Application')
 # # mail = outlook.CreateItem(0)
