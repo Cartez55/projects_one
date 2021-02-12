b816e754266205ed4bd8a897de4a8e3ec5e0e13d
18411029 <rvvagner@sberbank.ru>
2021-02-12 14:53:39 +0600

    Final changes in Parser. Involved: 1. Write in post 2. Send post

--- Parser/Parser_CSV.py
+++ Parser/Parser_CSV.py
@@ -1,22 +1,26 @@
 import csv
 import win32com.client as win32
 
-with open("export_r.csv", 'r', newline="", encoding='utf-8') as file:
-    # Считываем файл, указывая делитель ";"
-    reader = csv.DictReader(file, delimiter=';')
-    # Проходимся циклом по строке дата
-    r = file
-with open("exp.txt", "w", encoding="UTF-8") as p:
-    for row in r:
-        # Выводим строки из поля 'Номер и название SC/FAQ' которые соотвествует Дате
-        if row["Дата"] == '20.01.2021':
-            p.write(row["Номер и название SC/FAQ"] + ' | ' + row["Описание изменений"] + "\n")
+# Открываем файл для чтения
+with open("export_1.csv", 'r', newline="", encoding="UTF-8") as fp:
+    # Читаем файл с помощью csv.DictReader
+    reader = csv.DictReader(fp, delimiter=';')
+    # Работа с письмом
+    outlook = win32.Dispatch('Outlook.Application')
+    # Создаем письмо
+    mail = outlook.CreateItem(0)
+    adress_client = ['rvvagner@sberbank.ru;', ' Vishnyakova.Ol.Al1@sberbank.ru;']
+    # Итерацией добавляем в письмо адрессатов
+    for i in adress_client:
+        mail.To = i
+        # print(i)
+    # Тема письма
+    mail.Subject = 'Автоматическая рассылка. Тест'
+    for row in reader:
+        # Добавляем строки из поля 'Номер и название SC/FAQ' которые соотвествует Дате
+        date = '11.02.2021'
+        if row["Дата"] == date:
+            mail.Body = 'Добрый день!' + '\v' + 'Это автоматическая рассылка!' + "\n\n" + "Изменения в сценариях за " + date + ':' + '\n\n' + \
+                        row["Номер и название SC/FAQ"] + '\v' + "____" + "\v\v" + row["Описание изменений"] + '\n\n'
+    mail.Send()
 
-# outlook = win32.Dispatch('Outlook.Application')
-# # mail = outlook.CreateItem(0)
-# # mail.To = 'rvvagner@sberbank.ru'
-# # mail.Subject = 'Тест. Тест'
-# # with open("C:\\Users\\18411029\\Documents\\PythonCourses\\csv\\exp.csv", 'r', newline="", encoding='utf-8') as fp:
-# #     read = csv.reader(fp)
-# #     mail.Body = read
-# # mail.Send()
