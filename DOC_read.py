e0dd381db7ccca93efe2338decafbe2b9de76fdd
Vagner Roman <cartez55@gmail.com>
2021-02-21 16:25:39 +0600

    Doc_reader: experiment

--- DOC_read.py
+++ DOC_read.py
@@ -1,12 +1,13 @@
-import csv
+import docx
 
-with open("export_r.csv", 'r', newline="",
-          encoding='utf-8') as file:
-    # Считываем файл, указывая делитель ";"
-    reader = csv.DictReader(file, delimiter=';')
-    # Проходимся циклом по строке дата
-    with open("exp.txt", "w", encoding="UTF-8") as p:
-        for row in reader:
-            # Выводим строки из поля 'Номер и название SC/FAQ' которые соотвествует Дате
-            if row["Дата"] == '20.01.2021':
-                p.write(row["Номер и название SC/FAQ"] + ' | ' + row["Описание изменений"] + "\n")
+
+
+# with open("Parser\\export_r.csv", 'r', newline="", encoding='utf-8') as file:
+#     # Считываем файл, указывая делитель ";"
+#     reader = csv.DictReader(file, delimiter=';')
+#     # Проходимся циклом по строке дата
+#     with open("Parser\\exp.txt", "w", encoding="UTF-8") as p:
+#         for row in reader:
+#             # Выводим строки из поля 'Номер и название SC/FAQ' которые соотвествует Дате
+#             if row["Дата"] == '12.02.2021':
+#                 p.write(row["Номер и название SC/FAQ"] + ' | ' + row["Описание изменений"] + "\n")
