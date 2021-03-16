4b97224b452541acae74afed98ea38905fb9e916
18411029 <rvvagner@sberbank.ru>
2021-03-16 18:42:28 +0600

    add function join phrase

--- Text_Analysis_functions.py
+++ Text_Analysis_functions.py
@@ -1,12 +1,24 @@
 import re
+from random import choice
 
 # ПОИСК СЛОВ ПРИ ПОМОЩИ REGEXP
 
-ls = []
+# ls = []
 
 # Регулярка проверяется на сайте https://regex101.com/
 
-for i in ls:
-    gr = re.search("(уз....)|(полу....)|(най..)|(соз....)|(подключи..)|(уточ....)|(увид...)|(посм......)|(взя..)", i)
-    if gr:
-          print(gr.group())
+# for i in ls:
+#     gr = re.search("(уз....)|(полу....)|(най..)|(соз....)|(подключи..)|(уточ....)|(увид...)|(посм......)|(взя..)", i)
+#     if gr:
+#           print(gr.group())
+
+
+# Рандомная комбинация текста
+
+# phrase1 = []
+# phrase2 = []
+#
+#
+# for i in phrase1:
+#     for b in phrase2:
+#         print("Как",i,b)
