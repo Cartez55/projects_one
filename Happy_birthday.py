5e2484baf0d5232586acae6ad6ba51a04eb69642
18411029 <rvvagner@sberbank.ru>
2021-03-18 19:39:49 +0600

    fix function loop join phrase

--- Happy_birthday.py
+++ Happy_birthday.py
@@ -2,19 +2,28 @@ from datetime import *
 
 name = []
 date = []
+name_date = {}
 
-i
-while True:
-    name.append(input('Name: '))
-    date.append(input('Date: '))
+button_off = True
 
 
-    name_date = {}
+def write_name():
+    while button_off:
+        controll = input('Запишем ещё?:')
+        if controll == "нет":
+            print(name_date)
+            break
+        elif controll == "да":
+            name.append(input('Name: '))
+            date.append(input('Date: '))
 
-    for i in range(0, len(name)):
-        name_date[name[i]] = date[i]
 
+for i in range(0, len(name)):
+    name_date[name[i]] = date[i]
+
+
+print(name_date)
 # Проверка
-# print(name_date['Вагнер'])
+write_name()
 # print(name[0])
-print(type(date[0]))
+# print(type(date[0]))
