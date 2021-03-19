8fcb8330df63ce414654ad0a6511fcfac98856bf
18411029 <rvvagner@sberbank.ru>
2021-03-19 15:41:13 +0600

    add new function (find word in list words with 2 methods) in file

--- Text_Analysis_functions.py
+++ Text_Analysis_functions.py
@@ -1,5 +1,6 @@
 import re
 from random import choice
+import json
 
 # ПОИСК СЛОВ ПРИ ПОМОЩИ REGEXP
 
@@ -8,17 +9,34 @@ from random import choice
 # Регулярка проверяется на сайте https://regex101.com/
 
 # for i in ls:
-#     gr = re.search("(уз....)|(полу....)|(най..)|(соз....)|(подключи..)|(уточ....)|(увид...)|(посм......)|(взя..)", i)
+#     gr = re.search("[a-z]", i)
 #     if gr:
-#           print(gr.group())
+#         print(gr.group())
+
+# ПОИСК СЛОВ ИЗ СПИСКА
+
+for file in glob.glob("*.json"):
+    scenarios_dir = file
+
+with open(scenarios_dir, "r", encoding="utf-8") as scenario:
+    data = json.load(scenario)
+    guid = list(data["scenario"].keys())[0]
+    numb = guid[5:10]
+
+
+word_find = ['wor', 'goo']
+
+welcome = ["Hello world! Goodbye world!", "Ted Nelson, the inventor of hypertext",
+           "This book was more a stream-of-consciousness collage than anything else"]
+f = []
+
+
 
 
 # Рандомная комбинация текста
-
 # phrase1 = []
 # phrase2 = []
 #
-#
 # for i in phrase1:
 #     for b in phrase2:
-#         print("Как",i,b)
+#         print("Как", i, b)
