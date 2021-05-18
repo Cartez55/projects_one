b4dee5a2ece67a5c05cf11a94a1dd3cdcf8893e9
Cartez55 <cartez55@gmail.com>
2021-05-18 22:47:32 +0600

    add new text and example code about dict

--- List and Dict.py
+++ List and Dict.py
@@ -48,10 +48,54 @@ import random
 # empty_dict = dict()
 
 # Проверка наличия ключа в словаре через оператор in -> 'imutable' in example_dict
-# Добавление элемента в словарь exaple_dict['New_key'] = 'New_value' (var1)
-# Добавление элемента в словарь exaple_dict.update({'New_key' : 'New_value'}) (var2)
-# Удаление элемента из словаря del exaple_dict['Name_key'] (var1)
-# Удаление элемента из словаря del exaple_dict.pop('Name_key') (var2)
+# Добавление элемента в словарь example_dict['New_key'] = 'New_value' (var1)
+# Добавление элемента в словарь example_dict.update({'New_key' : 'New_value'}) (var2)
+# Удаление элемента из словаря del example_dict['Name_key'] (var1)
+# Удаление элемента из словаря del example_dict.pop('Name_key') (var2)
 # Проверка ключа 'Key' и добавление если нет в словаре - ключ, значение exaple_dict = {} -> exaple_dict.setdefault('Key', 'New_value')
-#
+# Итерация по словарю по ключу или по значению:
+# example_dict = {
+#     1: 'dsdsd',
+#     2: 'rgfgd'
+# }
 
+# for key in example_dict:
+    # print(key)
+# Итерация по элементам(key, value) с помощью метода items(), key - keys(), value - values()
+
+# for value in example_dict.values():
+    # print('{}'.format(value))
+
+# _____________________
+
+zen_python_text = '''Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+'''
+my_word = '''dfdf sdsd lol'''
+zen_map = dict()
+# Разбиваем строки на слова по пробелу split()
+for word in zen_python_text.split():
+    # Удаляем символы в строке (strip)
+    clean_word = word.strip('!,.').lower()
+    # Если
+    if word not in zen_map:
+        zen_map[clean_word] = 0
+    else:
+        zen_map.update(word)
