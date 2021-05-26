8af0be31502c68221b9482dfbd281efcc6277e48
Roman_Wagner <rvvagner@sberbank.ru>
2021-05-26 18:50:23 +0600

    add parser new v

--- Parser/CSV_Reader.py
+++ Parser/CSV_Reader.py
@@ -7,8 +7,16 @@ with open("CSI диалоги апрель.csv", 'r', newline="",
     # Проходимся циклом по строке дата
     with open("exp.txt", "w", encoding="UTF-8") as p:
         for row in reader:
-            # Выводим строки из поля 'Номер и название SC/FAQ' которые соотвествует Дате
             if row["CHAT_TXT"]:
                 row_split = row["CHAT_TXT"].split('!.')
-                print(row_split, "\n\n")
-            # p.write(row["CHAT_TXT"] + "\n\n")
+                # print(row_split, "\n\n")
+                # p.write(row["CHAT_TXT"] + "\n\n")
+                bot = []
+                human = []
+                all_phrase = []
+                list_symbols = ['!', '?', '.']
+                for symbols in list_symbols:
+                    for strings in row_split:
+                        if symbols not in strings:
+                            print(True)
+                            all_phrase.append(human.append(strings))
