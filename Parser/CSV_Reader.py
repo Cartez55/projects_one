2f5efc94ca085d031ffcb98e7e04fec31dc8f7fe
Cartez55 <cartez55@gmail.com>
2021-05-26 22:04:46 +0600

    add test file, write new strings in CSV_Reader

--- Parser/CSV_Reader.py
+++ Parser/CSV_Reader.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 with open("CSI диалоги апрель.csv", 'r', newline="",
           encoding='utf-8') as file:
@@ -8,15 +9,18 @@ with open("CSI диалоги апрель.csv", 'r', newline="",
     with open("exp.txt", "w", encoding="UTF-8") as p:
         for row in reader:
             if row["CHAT_TXT"]:
-                row_split = row["CHAT_TXT"].split('!.')
-                # print(row_split, "\n\n")
-                # p.write(row["CHAT_TXT"] + "\n\n")
+                # unique_symbols = set(row["CHAT_TXT"])
+                # print(unique_symbols)
+                # print(row["CHAT_TXT"].split('!,?'), "\n\n")
+
+                # print(re.split('!,?,.', row["CHAT_TXT"]), "\n\n")
+                # p.write(row["CHAT_TXT"] + "\n\n"
                 bot = []
                 human = []
                 all_phrase = []
-                list_symbols = ['!', '?', '.']
-                for symbols in list_symbols:
-                    for strings in row_split:
-                        if symbols not in strings:
-                            print(True)
-                            all_phrase.append(human.append(strings))
+                for strings in row["CHAT_TXT"]:
+                    if len(strings) > 40:
+                        bot.append(strings)
+                    else:
+                        human.append(strings)
+            # print(bot, "\n")
