57c968256d4f5135a7da63bdb9d45e56a30b226d
Roman_Wagner <rvvagner@sberbank.ru>
2021-05-27 18:56:14 +0600

    add text in parser

--- Parser/CSV_Reader.py
+++ Parser/CSV_Reader.py
@@ -8,20 +8,20 @@ with open("CSI диалоги апрель.csv", 'r', newline="",
     # Проходимся циклом по строке дата
     with open("exp.txt", "w", encoding="UTF-8") as p:
         for row in reader:
-            search_letter = re.search("[А-Я]", row["CHAT_TXT"])
-            if search_letter:
-                print(search_letter.group())
-                # unique_symbols = set(row["CHAT_TXT"])
-                # print(unique_symbols)
-                # print(re.split('[А-Я]', row["CHAT_TXT"]), "\n\n")
-                # print(re.split('!,?,.', row["CHAT_TXT"]), "\n")
-                # p.write(row["CHAT_TXT"] + "\n\n"
-                bot = []
-                human = []
-                all_phrase = []
-                for strings in row["CHAT_TXT"]:
-                    if len(strings) > 40:
-                        bot.append(strings)
-                    else:
-                        human.append(strings)
+            # search_letter =
+            if re.search("[А-Я]", row["CHAT_TXT"]):
+                print(row["CHAT_TXT"], "\n")
+            # unique_symbols = set(row["CHAT_TXT"])
+            # print(unique_symbols)
+            # print(re.split('[А-Я]', row["CHAT_TXT"]), "\n\n")
+            # print(re.split('!,?,.', row["CHAT_TXT"]), "\n")
+            # p.write(row["CHAT_TXT"] + "\n\n"
+            bot = []
+            human = []
+            all_phrase = []
+            for strings in row["CHAT_TXT"]:
+                if len(strings) > 40:
+                    bot.append(strings)
+                else:
+                    human.append(strings)
             # print(bot, "\n")
