726802e43771dc12ebd7bd1f39b39b6f54168b07
Roman_Wagner <rvvagner@sberbank.ru>
2021-05-31 12:50:12 +0600

    solutions new - a lot half work is great!

--- Kata_list.py
+++ Kata_list.py
@@ -2,20 +2,34 @@
 
 def tickets(people):
     # print(people[-2])
+    balance = []
+    short_change = []
     for index in range(0, len(people)):
-        if people[index] == 50 or 100:
-            int(people[index]) - 25
-            if len(people) <= 3:
-                if int(people[-1]) // 2 == 25:
-                    print("YES")
-                else:
-                    print("NO")
-            else:
-                print(int(people[index]) + int(people[-2]))
-                if int(people[index]) + int(people[-2]) == int(people[-1]) - 25:
-                    print("YES")
-                else:
-                    print("NO")
+        if people[index] == 50 or people[index] == 100:
+            short_change.append(people[index])
+        else:
+            balance.append(people[index])
+    print(f"balance: {balance} | short_change : {short_change}")
 
+    for cash in range(0, len(balance)):
+        for pocket_money in range(0, len(short_change)):
+            short_change.append(short_change[pocket_money] - 25)
+            balance.append(short_change[pocket_money] - balance[cash])
+    print(f"balance: {balance} | short_change : {short_change}")
 
-tickets(([25, 25, 50]))
+    # list_bank.append(int(people[index]) - 25)
+#
+#     # if len(people) <= 3:
+#     #     if int(people[-1]) // 2 == 25:
+#     #         print("YES")
+#     #     else:
+#     #         print("NO")
+#     # else:
+#     #     print(int(people[index]) + int(people[-2]))
+#     #     if int(people[index]) + int(people[-2]) == int(people[-1]) - 25:
+#     #         print("YES")
+#     #     else:
+#     #         print("NO")
+#
+#
+tickets(([25, 25, 50, 100]))
